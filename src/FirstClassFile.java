2e7566ef0131d097b404cdeac0e2493e1ce5a9b3
Luan Hajzeraj <luan.hajzeraj@student.uni-kassel.de>
2018-10-17 16:02:31 +0200

    Achter Commit - Notwendig fuer Merge

--- src/FirstClassFile.java
+++ src/FirstClassFile.java
@@ -4,5 +4,7 @@
 public class FirstClassFile {
     private int count =0;
     private int secondCount = 0;
+    private int variableBranch = 0;
+    private int zweiteVariableBranch = 0;
     private int x;
 }
