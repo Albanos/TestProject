096dccc4310c547effb483c4f67b384426da6227
Luan Hajzeraj <luan.hajzeraj@student.uni-kassel.de>
2018-10-19 12:32:30 +0200

    Genereller commit

--- src/FirstClassFile.java
+++ src/FirstClassFile.java
@@ -2,5 +2,4 @@
  * @author Luan Hajzeraj on 17.10.2018.
  */
 public class FirstClassFile {
-
 }
--- src/SecondClassFile.java
+++ src/SecondClassFile.java
@@ -2,15 +2,5 @@
  * @author Luan Hajzeraj on 17.10.2018.
  */
 public class SecondClassFile {
-    private int a;
-    private int b;
-    private int c;
 
-    public void ersteMethodeFuerBranch(){
-
-    }
-
-    public void zweiteMethodeFuerBranch(){
-
-    }
 }
