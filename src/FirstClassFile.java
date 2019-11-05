932c2f906f7f79b39047b9037c9aaaf543e84c9b
Luan Hajzeraj <luan.hajzeraj@student.uni-kassel.de>
2019-11-05 19:41:28 +0100

    Revert "TestPush 05.11.2019"
    This reverts commit 5153b3eba13e4bbf4598279ecd1a4a14cc1b7310.

--- src/FirstClassFile.java
+++ src/FirstClassFile.java
@@ -5,7 +5,5 @@ public class FirstClassFile {
     public void ersteMethodeMaster(){}
     public void zweiteMethodeMaster(){}
     public void dritteMethodeMaster(){}
-    public void vierteMethodeMaster{}
-    
 
 }
